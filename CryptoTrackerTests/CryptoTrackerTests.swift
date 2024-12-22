0055c65560a0febacd45aa4d5e1eb5e0f5b420d4
Javier Martin <javimartin@usal.es>
2024-12-22 20:18:56 +0100

    initial

---
+++ CryptoTrackerTests/CryptoTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerTests.swift
+//  CryptoTrackerTests
+//
+//  Created by Javier Martin on 22/12/24.
+//
+
+import Testing
+@testable import CryptoTracker
+
+struct CryptoTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
